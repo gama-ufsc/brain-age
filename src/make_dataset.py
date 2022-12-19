b74592d8fc413b07eccf28be1ed5660d006e31d9
victor-hro <victoroliveira.eng@hotmail.com>
2022-12-19 11:49:22 +0000

    prepare fixed split dataset with ADNI23+1

--- src/make_dataset.py
+++ src/make_dataset.py
@@ -19,10 +19,9 @@ import nibabel as nib
 from nibabel.processing import conform
 
 
-PREP_DATA_DIR = Path('/home/jupyter/gama/bruno/data/raw/ADNI1_prep')
-DATASET_FPATH = Path('/home/jupyter/gama/bruno/data/interim/ADNI1_slices_fix_2mm_5fold.hdf5')
-
-# SPLIT_CSV_FPATH = Path('/home/bruno-pacheco/brain-age/notebooks/dataframe3D.csv')
+ADNI1_DATA_DIR = Path('/home/jupyter/gama/bruno/data/raw/ADNI1_prep')
+ADNI23_DATA_DIR = Path('/home/jupyter/gama/bruno/data/raw/ADNI23_prep')
+DATASET_FPATH = Path('/home/jupyter/gama/bruno/data/interim/ADNI123_slices_fix_2mm_split.hdf5')
 
 DOWNSIZE = True
 
@@ -63,99 +62,142 @@ def load_preprocess_for_nnunet(
     return padded_brain
 
 if __name__ == '__main__':
-    assert PREP_DATA_DIR.exists(), f"`{PREP_DATA_DIR}` doesn't exist"
+    assert ADNI1_DATA_DIR.exists(), f"`{ADNI1_DATA_DIR}` doesn't exist"
+    assert ADNI23_DATA_DIR.exists(), f"`{ADNI23_DATA_DIR}` doesn't exist"
     assert DATASET_FPATH.parent.exists(), f"`{DATASET_FPATH.parent}` doesn't exist"
-
+    
     tmpdir.mkdir(exist_ok=True)
 
 #     assert SPLIT_CSV_FPATH.exists(), f"`{SPLIT_CSV_FPATH}` doesn't exist"
 
     target_shape = (80, 192, 160)
 
-    with open(PREP_DATA_DIR/'ages.pkl', 'rb') as f:
-        ages = pickle.load(f)
+    with open(ADNI1_DATA_DIR/'groups.pkl', 'rb') as f:
+        adni1_groups = pickle.load(f)
+
+    with open(ADNI1_DATA_DIR/'ages.pkl', 'rb') as f:
+        adni1_ages = pickle.load(f)
+
+    
+    with open(ADNI23_DATA_DIR/'groups.pkl', 'rb') as f:
+        adni23_groups = pickle.load(f)
+
+    with open(ADNI23_DATA_DIR/'ages.pkl', 'rb') as f:
+        adni23_ages = pickle.load(f)
+
+    ages = dict()
+    for k,v in adni1_ages.items():
+        ages[k] = v
+    for k,v in adni23_ages.items():
+        ages[k] = v
 
     if DOWNSIZE:
         target_shape = (40, 96, 96)
 
-#     # split data
-#     df = pd.read_csv(SPLIT_CSV_FPATH)
-#     train_sids = df[df['split'] == 'train']['patient'].unique()
-#     val_sids = df[df['split'] == 'val']['patient'].unique()
-#     test_sids = df[df['split'] == 'test']['patient'].unique()
+    adni1_fpaths = list(ADNI1_DATA_DIR.glob(f"*.nii"))
+    adni23_fpaths = list(ADNI23_DATA_DIR.glob(f"*.nii"))
 
-#     train_fpaths_ = [list(PREP_DATA_DIR.glob(f"{sid[5:]}*.nii")) for sid in train_sids]
-#     train_fpaths = list()
-#     for fs in train_fpaths_:
-#         train_fpaths += fs  # add all images from the subjects
-#     train_fpaths = sorted(train_fpaths)
+    assert len(adni1_ages.keys()) == len(adni1_fpaths)
+    assert len(adni23_ages.keys()) == len(adni23_fpaths)
 
-#     val_fpaths_ = [list(PREP_DATA_DIR.glob(f"{sid[5:]}*.nii")) for sid in val_sids]
-#     val_fpaths = list()
-#     for fs in val_fpaths_:
-#         val_fpaths += fs  # add all images from the subjects
-#     val_fpaths = sorted(val_fpaths)
+    # pick only CN
+    adni1_fpaths = [fp for fp in adni1_fpaths if adni1_groups[fp.name.split('__')[1].rstrip('.nii')] == 'CN']
+    adni23_fpaths = [fp for fp in adni23_fpaths if adni23_groups[fp.name.split('__')[1].rstrip('.nii')] == 'CN']
 
-#     test_fpaths_ = [list(PREP_DATA_DIR.glob(f"{sid[5:]}*.nii")) for sid in test_sids]
-#     test_fpaths = list()
-#     for fs in test_fpaths_:
-#         test_fpaths += fs  # add all images from the subjects
-#     test_fpaths = sorted(test_fpaths)
+    # split data
+    df_cv = pd.read_csv('/home/jupyter/gama/bruno/data/external/CROSSVAL.csv')
+    df_cv = df_cv.set_index('RID')
 
-#     i_train = 0
-#     i_test = 0
-#     i_val = 0
+    train_fpaths = adni23_fpaths
+    
+    val_fpaths = [fp for fp in adni1_fpaths if df_cv.loc[int(fp.name.split('__')[0].split('_S_')[1])]['TRAINING'] == 1]
+    
+    test_fpaths = [fp for fp in adni1_fpaths if df_cv.loc[int(fp.name.split('__')[0].split('_S_')[1])]['TRAINING'] == 0]
 
-    all_fpaths = list(PREP_DATA_DIR.glob(f"*.nii"))
+    i_train = 0
+    i_test = 0
+    i_val = 0
 
-    assert len(ages.keys()) == len(all_fpaths)
+#     # make splits
+#     splitter = KFold(5, shuffle=True, random_state=42)
 
-    # make splits
-    splitter = KFold(5, shuffle=True, random_state=42)
+#     sids = np.array(list({path.name.split('__')[0] for path in all_fpaths}))
 
-    sids = np.array(list({path.name.split('__')[0] for path in all_fpaths}))
+#     folds_fpaths = list()
+#     for _, fold_ix in splitter.split(sids):
+#         fold_sids = sids[fold_ix]
 
-    folds_fpaths = list()
-    for _, fold_ix in splitter.split(sids):
-        fold_sids = sids[fold_ix]
+#         fold_fpaths = np.array([path for path in all_fpaths if path.name.split('__')[0] in fold_sids])
 
-        fold_fpaths = np.array([path for path in all_fpaths if path.name.split('__')[0] in fold_sids])
+#         folds_fpaths.append(fold_fpaths)
 
-        folds_fpaths.append(fold_fpaths)
-
-    i_folds = [0,] * len(folds_fpaths)
+#     i_folds = [0,] * len(folds_fpaths)
 
     # create dataset
     if DATASET_FPATH.exists():
         # check if there's any progress already
-        for i, fold_fpaths in enumerate(folds_fpaths):
-            with h5py.File(DATASET_FPATH, 'r') as h:
-                # overwrite the last image just to be sure
-                n = max((h[str(i)]['y'].shape[0] // target_shape[0]) - 1,0)
+        with h5py.File(DATASET_FPATH, 'r') as h:
+            # always overwrite the last image just to be sure
+            n_train = max((h['train']['y'].shape[0] // target_shape[0]) - 1,0)
+            n_val = max((h['val']['y'].shape[0] // target_shape[0]) - 1,0)
+            n_test = max((h['test']['y'].shape[0] // target_shape[0]) - 1,0)
 
-            fold_fpaths = fold_fpaths[n-1:]
+        train_fpaths = train_fpaths[n_train-1:]
+        val_fpaths = val_fpaths[n_val-1:]
+        test_fpaths = test_fpaths[n_test-1:]
 
-            folds_fpaths[i] = (train_fpaths, test_fpaths)
-            i_folds[i] = n * target_shape[0]
+        i_train = n_train * target_shape[0]
+        i_val = n_val * target_shape[0]
+        i_test = n_test * target_shape[0]
     else:
-        with h5py.File(DATASET_FPATH, 'w') as h:            
-            for i, _ in enumerate(folds_fpaths):
-                fold = h.create_group(str(i))
+        with h5py.File(DATASET_FPATH, 'w') as h:
+            train = h.create_group('train')
+            X = train.create_dataset(
+                'X',
+                (0,target_shape[1],target_shape[2]),
+                maxshape=(None,target_shape[1],target_shape[2]),
+                dtype='float32',
+                chunks=(1,target_shape[1],target_shape[2]),
+                compression='gzip',
+            )
+            y = train.create_dataset(
+                'y',
+                (0,),
+                maxshape=(None,),
+                dtype='float32',
+            )
 
-                X = fold.create_dataset(
-                    'X',
-                    (0,target_shape[1],target_shape[2]),
-                    maxshape=(None,target_shape[1],target_shape[2]),
-                    dtype='float32',
-                    chunks=(1,target_shape[1],target_shape[2]),
-                    compression='gzip',
-                )
-                y = fold.create_dataset(
-                    'y',
-                    (0,),
-                    maxshape=(None,),
-                    dtype='float32',
-                )
+            val = h.create_group('val')
+            X = val.create_dataset(
+                'X',
+                (0,target_shape[1],target_shape[2]),
+                maxshape=(None,target_shape[1],target_shape[2]),
+                dtype='float32',
+                chunks=(1,target_shape[1],target_shape[2]),
+                compression='gzip',
+            )
+            y = val.create_dataset(
+                'y',
+                (0,),
+                maxshape=(None,),
+                dtype='float32',
+            )
+
+            test = h.create_group('test')
+            X = test.create_dataset(
+                'X',
+                (0,target_shape[1],target_shape[2]),
+                maxshape=(None,target_shape[1],target_shape[2]),
+                dtype='float32',
+                chunks=(1,target_shape[1],target_shape[2]),
+                compression='gzip',
+            )
+            y = test.create_dataset(
+                'y',
+                (0,),
+                maxshape=(None,),
+                dtype='float32',
+            )
 
     def update_dataset(imgs_fpaths, i, ds_name):
         for img_fpath in tqdm(imgs_fpaths):
@@ -187,6 +229,6 @@ if __name__ == '__main__':
             else:
                 img_fpath.unlink()
 
-    for i, fold_fpaths in enumerate(folds_fpaths):
-        print(f"Working on fold {i}")
-        update_dataset(fold_fpaths, i_folds[i], str(i))
+    for ds in ['train', 'val', 'test']:
+        print(f"Working on {ds} dataset")
+        update_dataset(eval(f"{ds}_fpaths"), eval(f"i_{ds}"), ds)
