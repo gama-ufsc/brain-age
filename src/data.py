c9765da0b3ed5e3032611907548873bad2c9e681
victor-hro <victoroliveira.eng@hotmail.com>
2022-12-19 11:48:43 +0000

    refactor KFold dataset

--- src/data.py
+++ src/data.py
@@ -8,7 +8,7 @@ from torch.utils.data import Dataset
 from torchvision import transforms
 
 
-class ADNIDatasetForBraTSModel(Dataset):
+class ADNIDataset(Dataset):
     def __init__(
         self,
         hdf5_fpath,
@@ -59,26 +59,77 @@ class ADNIDatasetForBraTSModel(Dataset):
 
         return img, label
 
-class ADNISemiSupervisedDataset(ADNIDatasetForBraTSModel):
-    def __getitem__(self, index: int):
-        if self.dataset == 'train+val':
-            with h5py.File(self._fpath, 'r') as h:
-                train_len = h['train']['y'].shape[0]
-            index_ = index - train_len
+class ADNIDatasetKFold(Dataset):
+    def __init__(
+        self,
+        hdf5_fpath,
+        transform=torch.Tensor,
+        dataset='train',
+        split=0,
+    ) -> None:
+        super().__init__()
 
-            if index_ < 0:
-                dataset = 'train'
+        hdf5_fpath = Path(hdf5_fpath)
+        assert hdf5_fpath.exists()
+
+        self._fpath = hdf5_fpath
+        
+        assert dataset.lower() in ['train', 'test'], '`dataset` must be either `train` or `test`.'
+        self.dataset = dataset.lower()
+        self.split = split
+
+        self.transform = transform
+
+    def __len__(self):
+        with h5py.File(self._fpath, 'r') as h:
+            if self.dataset == 'test':
+                l = h[str(self.split)]['y'].shape[0]
             else:
-                dataset = 'val'
+                l = 0
+                for i in range(5):
+                    if i != self.split:
+                        l += h[str(i)]['y'].shape[0]
+
+        return l
+
+    def __getitem__(self, index: int):
+        if self.dataset == 'train':
+            splits = [i for i in range(5) if i != self.split]
+            sizes = list()
+            for i in splits:
+                with h5py.File(self._fpath, 'r') as h:
+                    sizes.append(h[str(i)]['y'].shape[0])
+
+            full_len = self.__len__()
+            assert sum(sizes) == full_len
+            if index >= full_len:
+                raise IndexError
+
+            for i, size in enumerate(sizes):
+                index = index - size
+                split = splits[i]
+                if index < 0:
+                    break
         else:
-            index_ = index
-            dataset = self.dataset
+            split = self.split
 
         with h5py.File(self._fpath, 'r') as h:
-            img = h[dataset]['X'][index_]
-            label = h[dataset]['y'][index_]
-        
-        i_slice = index_ % 40
+            img = h[str(split)]['X'][index]
+            label = h[str(split)]['y'][index]
+
+        # transform
+        if self.transform is not None:
+            img = self.transform(img)
+
+        return img, label
+    
+class ADNISemiSupervisedDataset(ADNIDataset):
+    def __getitem__(self, index: int):
+        with h5py.File(self._fpath, 'r') as h:
+            img = h[str(self.split)]['X'][index]
+            label = h[str(self.split)]['y'][index]
+
+        i_slice = index % 40
 
         # transform
         if self.transform is not None:
