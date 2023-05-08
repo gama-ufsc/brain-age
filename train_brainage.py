1b287937e47904ce0472a77cb05e21e74fc9af9a
Bruno M. Pacheco <mpacheco.bruno@gmail.com>
2023-05-08 17:01:34 +0000

    add final configurations

--- train_brainage.py
+++ train_brainage.py
@@ -31,8 +31,10 @@ if __name__ == '__main__':
     if args[-1] == '--all':
         s = 'train+val'
         args = args[:-1]
+        wandb_group = ''
     else:
         s = 'train'
+        wandb_group = 'val_'
 
     try:
         run_n = int(args[-1])
@@ -69,14 +71,17 @@ if __name__ == '__main__':
     )
 
     if model == 'unet':  ### UNet ###
-        wandb_group = 'val_UNet'
+        wandb_group += 'UNet'
         # load nnunet_trainer
         if pretrained == 'brats':
-            unet_brats_ids = ['ztd8d21k', '9ld37s4y', '2r3ii0b4', '2ebpcar2', 'qto04d2p']
+            unet_brats_ids = ['1nfcq3ju', '9ld37s4y', '2r3ii0b4', '2ebpcar2', 'qto04d2p']
             nnunet_trainer = load_nnunet_from_wandb(unet_brats_ids[run_n])
 #             nnunet_model_fpath = '/home/jupyter/gama/nnUNet/models/nnUNet/2d/Task102_BraTS2020/nnUNetTrainerV2__nnUNetPlansv2.1/fold_0/model_final_checkpoint.model.pkl'
 #             nnunet_trainer = restore_model(nnunet_model_fpath, checkpoint=nnunet_model_fpath[:-4], train=False)
 #             nnunet_trainer.initialize(training=False)
+
+            if s == 'train+val':
+                trainer_kwargs['epochs'] = 7
 
             wandb_group += '+BraTS'
         elif pretrained == 'old-brats':
@@ -93,6 +98,9 @@ if __name__ == '__main__':
             )
             nnunet_trainer.initialize(False)
 
+            if s == 'train+val':
+                trainer_kwargs['epochs'] = 18
+
         if pretrained != 'old-brats':
             # create brain age model with backbone from nnunet_trainer.network's encoder
             net = BraTSnnUNet(nnunet_trainer.network)
@@ -108,7 +116,7 @@ if __name__ == '__main__':
             **trainer_kwargs,
         ).run()
     elif model == 'resnet':  ### ResNet ###
-        wandb_group = 'val_ResNet'
+        wandb_group += 'ResNet'
         # load nnunet_trainer
         if pretrained == 'brats':
             resnet_brats_ids = ['6jvz00y4', '1ui6xsgh', '12dollka', '2q3478oi', '3fizs05h']
@@ -119,6 +127,9 @@ if __name__ == '__main__':
 #             nnunet_trainer.initialize(training=False)
 
 #             wandb_group += '+local_BraTS'
+
+            if s == 'train+val':
+                trainer_kwargs['epochs'] = 23
         elif pretrained == 'imagenet':
             # new trainer, to initialize ResNet backbone with ImageNet weights
             nnunet_trainer = nnUNetTrainerV2_ResNetUNet(
@@ -130,10 +141,16 @@ if __name__ == '__main__':
             )
             nnunet_trainer.initialize(False)
             wandb_group += '+ImageNet'
+            
+            if s == 'train+val':
+                trainer_kwargs['epochs'] = 25
         elif pretrained == 'imagenet+brats':
             resnet_imagenet_brats_ids = ['38xp1owi', '16x20wq1', 'e8gc6ih2', '3olfcxsq', '1hsptxr0']
             nnunet_trainer = load_nnunet_from_wandb(resnet_imagenet_brats_ids[run_n])
             wandb_group += '+ImageNet+BraTS'
+
+            if s == 'train+val':
+                trainer_kwargs['epochs'] = 38
         else:
             # new trainer, to initialize backbone with random weights
             nnunet_trainer = nnUNetTrainerV2_ResNetUNet(
@@ -143,6 +160,9 @@ if __name__ == '__main__':
                 **nnunet_trainer_kwargs,
             )
             nnunet_trainer.initialize(False)
+            
+            if s == 'train+val':
+                trainer_kwargs['epochs'] = 39
 
         # create brain age model with backbone from nnunet_trainer.network's encoder
         resnet_backbone = nnunet_trainer.network.encoder
--- train_brats_models.py
+++ train_brats_models.py
@@ -5,92 +5,93 @@ from nnunet.training.network_training.nnUNet_variants.architectural_variants.nnU
 
 
 if __name__ == '__main__':
-    if sys.argv[-1] == '--all':
+    args = sys.argv
+
+    if args[-1] == '--all':
         fold = 'all'
+        args = args[:-1]
     else:
         fold = 0
 
-    ### UNet ###
-#     trainer_kwargs = dict(
-#         output_folder='/home/jupyter/gama/nnUNet/models/nnUNet/2d/Task102_BraTS2020/nnUNetTrainerV2__nnUNetPlansv2.1',
-#         dataset_directory='/home/jupyter/gama/nnUNet/data/processed/Task102_BraTS2020',
-#         batch_dice=True,
-#         stage=0,
-#         unpack_data=True,
-#         deterministic=False,
-#         fp16=True,
-#     )
-#     validate_kwargs = dict(
-#         save_softmax=None,
-#         validation_folder_name='validation_raw',
-#         run_postprocessing_on_folds=True,
-#         overwrite=None,
-#     )
+    if args[-1].lower() == 'unet':  ### UNet ###
+        trainer_kwargs = dict(
+            output_folder='/home/jupyter/gama/nnUNet/models/nnUNet/2d/Task102_BraTS2020/nnUNetTrainerV2__nnUNetPlansv2.1',
+            dataset_directory='/home/jupyter/gama/nnUNet/data/processed/Task102_BraTS2020',
+            batch_dice=True,
+            stage=0,
+            unpack_data=True,
+            deterministic=False,
+            fp16=True,
+        )
+        validate_kwargs = dict(
+            save_softmax=None,
+            validation_folder_name='validation_raw',
+            run_postprocessing_on_folds=True,
+            overwrite=None,
+        )
 
-#     trainer = nnUNetTrainerV2(
-#         '/home/jupyter/gama/nnUNet/data/processed/Task102_BraTS2020/nnUNetPlansv2.1_plans_2D.pkl',
-#         fold, **trainer_kwargs
-#     )
+        trainer = nnUNetTrainerV2(
+            '/home/jupyter/gama/nnUNet/data/processed/Task102_BraTS2020/nnUNetPlansv2.1_plans_2D.pkl',
+            fold, **trainer_kwargs
+        )
 
-#     trainer.initialize(True)
+        trainer.initialize(True)
 
-#     trainer.run_training()
+        trainer.run_training()
 
-#     trainer.validate(**validate_kwargs)
+        trainer.validate(**validate_kwargs)
+    elif args[-1].lower() == 'resunet':  ### ResUNet ###
+        trainer_kwargs = dict(
+            output_folder='/home/jupyter/gama/nnUNet/models/nnUNet/2d/Task102_BraTS2020/nnUNetTrainerV2_ResNetUNet__nnUNetPlans_ResNetUNet_v2.1',
+            dataset_directory='/home/jupyter/gama/nnUNet/data/processed/Task102_BraTS2020',
+            batch_dice=True,
+            stage=0,
+            unpack_data=True,
+            deterministic=False,
+            fp16=True,
+        )
+        validate_kwargs = dict(
+            save_softmax=None,
+            validation_folder_name='validation_raw',
+            run_postprocessing_on_folds=True,
+            overwrite=None,
+        )
 
-    ### ResUNet ###
-#     trainer_kwargs = dict(
-#         output_folder='/home/jupyter/gama/nnUNet/models/nnUNet/2d/Task102_BraTS2020/nnUNetTrainerV2_ResNetUNet__nnUNetPlans_ResNetUNet_v2.1',
-#         dataset_directory='/home/jupyter/gama/nnUNet/data/processed/Task102_BraTS2020',
-#         batch_dice=True,
-#         stage=0,
-#         unpack_data=True,
-#         deterministic=False,
-#         fp16=True,
-#     )
-#     validate_kwargs = dict(
-#         save_softmax=None,
-#         validation_folder_name='validation_raw',
-#         run_postprocessing_on_folds=True,
-#         overwrite=None,
-#     )
+        trainer = nnUNetTrainerV2_ResNetUNet(
+            '/home/jupyter/gama/nnUNet/data/processed/Task102_BraTS2020/nnUNetPlans_ResNetUNet_v2.1_plans_2D.pkl',
+            fold, **trainer_kwargs
+        )
 
-#     trainer = nnUNetTrainerV2_ResNetUNet(
-#         '/home/jupyter/gama/nnUNet/data/processed/Task102_BraTS2020/nnUNetPlans_ResNetUNet_v2.1_plans_2D.pkl',
-#         fold, **trainer_kwargs
-#     )
+        trainer.initialize(True)
 
-#     trainer.initialize(True)
+        trainer.run_training()
 
-#     trainer.run_training()
+        trainer.validate(**validate_kwargs)
+    elif args[-1].lower() == 'pt-resunet':  ### Pre-trained ResUNet ###
+        trainer_kwargs = dict(
+            output_folder='/home/jupyter/gama/nnUNet/models/nnUNet/2d/Task102_BraTS2020/nnUNetTrainerV2_ResNetUNet__nnUNetPlans_ResNetUNet_v2.1',
+            dataset_directory='/home/jupyter/gama/nnUNet/data/processed/Task102_BraTS2020',
+            batch_dice=True,
+            stage=0,
+            unpack_data=True,
+            deterministic=False,
+            fp16=True,
+            pretrained_resnet=True,
+        )
+        validate_kwargs = dict(
+            save_softmax=None,
+            validation_folder_name='validation_raw',
+            run_postprocessing_on_folds=True,
+            overwrite=None,
+        )
 
-#     trainer.validate(**validate_kwargs)
+        trainer = nnUNetTrainerV2_ResNetUNet(
+            '/home/jupyter/gama/nnUNet/data/processed/Task102_BraTS2020/nnUNetPlans_ResNetUNet_v2.1_plans_2D.pkl',
+            fold, **trainer_kwargs
+        )
 
-    ### Pre-trained ResUNet ###
-    trainer_kwargs = dict(
-        output_folder='/home/jupyter/gama/nnUNet/models/nnUNet/2d/Task102_BraTS2020/nnUNetTrainerV2_ResNetUNet__nnUNetPlans_ResNetUNet_v2.1',
-        dataset_directory='/home/jupyter/gama/nnUNet/data/processed/Task102_BraTS2020',
-        batch_dice=True,
-        stage=0,
-        unpack_data=True,
-        deterministic=False,
-        fp16=True,
-        pretrained_resnet=True,
-    )
-    validate_kwargs = dict(
-        save_softmax=None,
-        validation_folder_name='validation_raw',
-        run_postprocessing_on_folds=True,
-        overwrite=None,
-    )
+        trainer.initialize(True)
 
-    trainer = nnUNetTrainerV2_ResNetUNet(
-        '/home/jupyter/gama/nnUNet/data/processed/Task102_BraTS2020/nnUNetPlans_ResNetUNet_v2.1_plans_2D.pkl',
-        fold, **trainer_kwargs
-    )
+        trainer.run_training()
 
-    trainer.initialize(True)
-
-    trainer.run_training()
-
-    trainer.validate(**validate_kwargs)
+        trainer.validate(**validate_kwargs)
